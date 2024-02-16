616340521dd8623dae00bcfef1c886bd1294bffd
Mkhosi99 <mkhosishabalala2@gmail.com>
2024-02-16 18:28:35 +0200

    Print square

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 /*
     Print a square with the character #
-    
-    The size of the square must be the first argument 
+
+    The size of the square must be the first argument
     of the program.
 */
 
@@ -14,25 +14,10 @@ if (process.argv.length <= 2) {
     process.exit(1)
 }
 
-/*
-   This line parses the first command-line argument (process.argv[2]) as an integer using parseInt(). 
-   This argument represents the size of the square to be printed.
-*/
-const size = parseInt(process.argv[2]);
+size = parseInt(process.argv[2], 10)
 
-/*
-   This block of code checks if the parsed size is NaN (Not-a-Number). 
-   If the argument is not a valid number, 
-   it prints an error message to the standard error stream indicating an invalid size argument, 
-   and exits the script with a status code of 1.
-*/
-if (isNaN(size)) {
-    process.stderr.write("Invalid size argument. Please provide a valid number.\n");
-    process.exit(1);
-}
-
-for (let i = 0 ; i < size ; i ++) {
-    for (let j = 0 ; j < size ; j ++) {
+for (let i = 0; i < size; i++) {
+    for (let j = 0; j < size; j++) {
         process.stdout.write("#");
     }
     process.stdout.write("\n");
