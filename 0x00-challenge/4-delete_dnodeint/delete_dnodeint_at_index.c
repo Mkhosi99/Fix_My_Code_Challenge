3cd9dbf10a3d4fcb9ffbf4a549dcf58bc961a10e
Mkhosi99 <mkhosishabalala2@gmail.com>
2024-02-18 01:59:11 +0200

    bypassing the current head node.

--- 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -43,7 +43,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	}
 	else
 	{
-		// bypassing the current head node.
+		/* bypassing the current head node */
 		(*head)->prev->next = (*head)->next;
 		free(*head);
 		if ((*head)->next)
